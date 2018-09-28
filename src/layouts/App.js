117a1aa249997a791a2e97f07f48352fc610250b
王哲 <moose@wangzhedeMacBook-Pro.local>
2018-09-29 00:54:28 +0800

    first page

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,4 +1,4 @@
-import react, { Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { Row, Col } from 'antd';
 import PropTypes from 'prop-types';
 import { connect } from 'dva';
@@ -16,9 +16,9 @@ const App = ({
 			<Header></Header>
 			<Content>
 				<Row>
-					<Col span={2}></Col>
-					<Col span={20}>{ children }</Col>
-					<Col span={2}></Col>
+					<Col span={5}></Col>
+					<Col span={14}>{ children }</Col>
+					<Col span={5}></Col>
 				</Row>
 			</Content>
 			<Footer></Footer>					
--- src/pages/first/index.js
+++ src/pages/first/index.js
@@ -1,16 +1,34 @@
 import React, { Fragment } from 'react';
 import { connect } from 'dva';
+import PropTypes from 'prop-types';
 import styles from './index.less';
+import { Row, Col } from 'antd';
+import TextScroll from 'react-textscroll';
 
-function First() {
+const First = ({ first }) => {
   return (    
     <Fragment>
-      first page
+      <Row>
+      	<Col span={24} className={styles.text_scrolling}>
+      	 <TextScroll mode='horizontal' text={first.text} speed={10000} />
+      	</Col>
+      </Row>
+      <Row gutter={24}>
+      	{first.cardList.map((item, key)=>{
+      		return(
+      			<Col key={key} span={8} className={styles.single}>     				     				
+      				<img alt={key} src={item.url}/>
+      				<div className={styles.mask}></div>
+      			</Col>
+      		)
+      	})}
+      </Row>
     </Fragment>
   );
 }
 
 First.propTypes = {
+	first: PropTypes.object,
 };
 
-export default connect()(First);
+export default connect(({ first }) => ({ first }))(First);
