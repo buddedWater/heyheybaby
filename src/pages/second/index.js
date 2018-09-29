ecf90f36faae98f943fdb3bee0df27f65f3f6ce5
buddedWater <32365556+buddedWater@users.noreply.github.com>
2018-09-29 18:20:11 +0800

    detail

--- src/pages/second/index.js
+++ src/pages/second/index.js
@@ -2,10 +2,11 @@ import React, { Fragment } from 'react';
 import { connect } from 'dva';
 import PropTypes from 'prop-types';
 import styles from './index.less';
-import { Row, Col, List } from 'antd';
+import { Row, Col, List, Icon } from 'antd';
 import orange from '../../assets/Orange.svg';
 import TweenOne from 'rc-tween-one';
 import { routerRedux } from 'dva/router';
+import QueueAnim from 'rc-queue-anim';
 
 const Second = ({ second, dispatch }) => {
 
@@ -17,14 +18,41 @@ const Second = ({ second, dispatch }) => {
 
   const renderItem = (item) => {
     return(
-      <List.Item>
-        <Row>
+      <List.Item key={item.key}>
+        <Row onClick={()=>showDetail(item)}>
           <Col span={24}>{item.content}</Col>
           <Col span={12}>{item.name}</Col>
           <Col span={12} className={styles.date}>{item.date}</Col>
         </Row>
       </List.Item>
     )    
+  }
+
+  const detailContent = () => {
+    return (
+      <Fragment>
+        {second.activeRow?<Row>
+          <Col span={24} className={styles.title}><h2>{second.activeRow.title}</h2></Col>       
+          <QueueAnim className="demo-content"
+            key="demo"
+            type={['right', 'left']}
+            ease={['easeOutQuart', 'easeInOutQuart']}>
+              {second.activeRow.content.trim().split(" ").map((item, key)=>{
+                return <Col className={styles.text_col} key={key} span={24} >{item}</Col>
+              })}                    
+          </QueueAnim>
+        </Row>:null}
+      </Fragment>     
+    )
+  }
+
+  const showDetail = (item) => {
+    dispatch({type:"second/updateState",payload:{show:false, activeRow:item}})
+    //dispatch({type:"second/query_detail",payload:{activeKey:item.key}})
+  }
+
+  const hideDetail = () => {
+    dispatch({type:"second/updateState",payload:{show:true}})
   }
 
   const handleTouch = (flag) => {
@@ -37,11 +65,19 @@ const Second = ({ second, dispatch }) => {
 
   return (    
     <Fragment>
-      <Row className={styles.second}>
+      <Row className={styles.second}>    
       	<Col span={24}>
-          <List {...listProps}></List>
+          <QueueAnim className="demo-content"
+            key="demo"
+            type={['right', 'left']}
+            ease={['easeOutQuart', 'easeInOutQuart']}>
+            {second.show?[<List {...listProps}/>]:detailContent()}           
+          </QueueAnim>
         </Col> 
-        <Col span={24} className={styles.rotate_image}>
+        <Col span={12} style={{padding:"30px 0"}}>
+          {second.show?null:<span className={styles.back} onClick={()=>hideDetail()}><Icon type="left" ></Icon>Back</span>}
+        </Col>
+        <Col span={12} className={styles.rotate_image}>
           <TweenOne style={{ display:'inline-block'}} paused={second.paused} animation={{   
               x: -400,           
               rotate: 360, 
