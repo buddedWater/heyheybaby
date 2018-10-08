ce8eccca3c4c7612358029856a25990ec0ec1771
buddedWater <32365556+buddedWater@users.noreply.github.com>
2018-10-08 17:30:00 +0800

    update grid

--- .webpackrc.js
+++ .webpackrc.js
@@ -3,6 +3,7 @@ export default {
   "proxy": {
     "/api": {
       "target": "http://127.0.0.1:3000/",
+      //"target": "http://60.205.231.78:3000/",
       "changeOrigin": true,
       "pathRewrite": { "^/api" : "" }
     }
--- src/layouts/App.js
+++ src/layouts/App.js
@@ -17,7 +17,7 @@ const App = ({
   children, dispatch, app, loading, location,
 }) => {
 
-  const offset = (location.pathname === "/" || location.pathname === "/owner" || location.pathname === "/operate") ? {xs:2, sm:4, md:4, lg:4, xl:5, xxl:6} : {xs:2, sm:4, md:4, lg:4, xl:4, xxl:1}
+  const offset = (location.pathname === "/" || location.pathname === "/owner" || location.pathname === "/operate") ? {xs:2, sm:4, md:4, lg:4, xl:5, xxl:6} : {xs:2, sm:4, md:3, lg:3, xl:3, xxl:2}
   const content = (location.pathname === "/" || location.pathname === "/owner" || location.pathname === "/operate") ? {xs:20, sm:16, md:16, lg:16, xl:14, xxl:12} : {xs:20, sm:14, md:12, lg:10, xl:9, xxl:8}
 
   const handleLogout = () => {
